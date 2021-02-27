c1c45ccaf977f75a1de047b2a85a25c5388514f3
WarWolfen <warwolfen@gmail.com>
2021-02-27 23:08:29 +0100

    Removed a couple of debug outputs.

---
+++ net.clanwolf.starmap.administration/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module net.clanwolf.starmap.administration {
+	requires java.desktop;
+
+	requires javafx.graphics;
+	requires javafx.controls;
+	requires javafx.media;
+	requires javafx.fxml;
+	requires java.logging;
+}
