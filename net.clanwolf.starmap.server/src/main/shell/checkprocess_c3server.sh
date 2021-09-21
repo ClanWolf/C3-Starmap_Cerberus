b6bedbd54e98203a5bf738df8ee238751329ca0f
WarWolfen <warwolfen@gmail.com>
2021-09-21 23:06:29 +0200

    Improved lobby for planetary invasions.

--- net.clanwolf.starmap.server/src/main/shell/checkprocess_c3server.sh
+++ net.clanwolf.starmap.server/src/main/shell/checkprocess_c3server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 test -f /var/www/vhosts/clanwolf.net/httpdocs/apps/C3/server/C3-Server_shutdown.flag && exit
-ps -fC java | grep C3-Server- || /opt/jdk-16.0.2/bin/java -jar /var/www/vhosts/clanwolf.net/httpdocs/apps/C3/server/C3-Server-5.3.1.jar ; exit
+ps -fC java | grep C3-Server- || /opt/jdk-16.0.2/bin/java -jar /var/www/vhosts/clanwolf.net/httpdocs/apps/C3/server/C3-Server-5.3.2.jar ; exit
 exit
