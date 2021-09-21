ac6804af7b7b5c998ff5d161471397b9abd74d3a
Christian Bartel <warwolfen@gmail.com>
2021-09-21 12:00:55 +0200

    Update C3GameSessionHandler.java

--- net.clanwolf.starmap.server/src/main/java/net/clanwolf/starmap/server/beans/C3GameSessionHandler.java
+++ net.clanwolf.starmap.server/src/main/java/net/clanwolf/starmap/server/beans/C3GameSessionHandler.java
@@ -327,6 +327,7 @@ public class C3GameSessionHandler extends SessionMessageHandler {
 
 			// TODO: Clear hibernate cache, refresh jumpship and routepoints
 			// https://stackoverflow.com/questions/5832415/entitymanager-refresh
+			//entityManager().flush();
 			//entityManager.clear();
 			
 			// https://stackoverflow.com/questions/27905148/force-hibernate-to-read-database-and-not-return-cached-entity
