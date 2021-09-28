1204ea94fb268db25a160f020c9fe8bd5c1d1980
WarWolfen <warwolfen@gmail.com>
2021-09-28 23:31:41 +0200

    Updating the universe and moving the jumpships along their routes.

--- net.clanwolf.starmap.server/src/main/shell/checkprocess_c3server.sh
+++ net.clanwolf.starmap.server/src/main/shell/checkprocess_c3server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 test -f /var/www/vhosts/clanwolf.net/httpdocs/apps/C3/server/C3-Server_shutdown.flag && exit
-ps -fC java | grep C3-Server- || /opt/jdk-16.0.2/bin/java -jar /var/www/vhosts/clanwolf.net/httpdocs/apps/C3/server/C3-Server-5.4.11.jar ; exit
+ps -fC java | grep C3-Server- || /opt/jdk-16.0.2/bin/java -jar /var/www/vhosts/clanwolf.net/httpdocs/apps/C3/server/C3-Server-5.4.17.jar ; exit
 exit
