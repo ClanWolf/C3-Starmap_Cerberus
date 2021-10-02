9964c93685faf04c116fec27d81b866b8784609f
WarWolfen <warwolfen@gmail.com>
2021-10-02 03:27:54 +0200

    Several fixes and changes, Java 17, global shortcuts, pretty screenshots and more.

--- NSIS/c3-client_createInstaller.cmd
+++ NSIS/c3-client_createInstaller.cmd
@@ -6,7 +6,7 @@ C:
 CD \
 CD C:\C3\projects\C3-Starmap_Cerberus
 
-REM SET VERSION=5.4.17
+REM SET VERSION=5.5.1
 FOR /f "delims== tokens=1,2" %%G in (C:\C3\projects\C3-Starmap_Cerberus\net.clanwolf.starmap.client\target\classes\version.number) do set %%G=%%H
 ECHO Found version: %VERSION%
 REM PAUSE
