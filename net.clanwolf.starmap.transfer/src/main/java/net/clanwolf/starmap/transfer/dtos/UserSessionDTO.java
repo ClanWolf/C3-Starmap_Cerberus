6d617b5ea95bb2b89079f570d99afe3e0b95949d
WarWolfen <warwolfen@gmail.com>
2022-02-04 00:25:01 +0100

    User sessions.

---
+++ net.clanwolf.starmap.transfer/src/main/java/net/clanwolf/starmap/transfer/dtos/UserSessionDTO.java
@@ -0,0 +1,98 @@
+/* ---------------------------------------------------------------- |
+ *    ____ _____                                                    |
+ *   / ___|___ /                   Communicate - Command - Control  |
+ *  | |     |_ \                   MK V "Cerberus"                  |
+ *  | |___ ___) |                                                   |
+ *   \____|____/                                                    |
+ *                                                                  |
+ * ---------------------------------------------------------------- |
+ * Info        : https://www.clanwolf.net                           |
+ * GitHub      : https://github.com/ClanWolf                        |
+ * ---------------------------------------------------------------- |
+ * Licensed under the Apache License, Version 2.0 (the "License");  |
+ * you may not use this file except in compliance with the License. |
+ * You may obtain a copy of the License at                          |
+ * http://www.apache.org/licenses/LICENSE-2.0                       |
+ *                                                                  |
+ * Unless required by applicable law or agreed to in writing,       |
+ * software distributed under the License is distributed on an "AS  |
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either  |
+ * express or implied. See the License for the specific language    |
+ * governing permissions and limitations under the License.         |
+ *                                                                  |
+ * C3 includes libraries and source code by various authors.        |
+ * Copyright (c) 2001-2022, ClanWolf.net                            |
+ * ---------------------------------------------------------------- |
+ */
+package net.clanwolf.starmap.transfer.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import net.clanwolf.starmap.transfer.Dto;
+
+import java.sql.Timestamp;
+
+/**
+ * UserSessionPOJO entity.
+ * 
+ * @author MyEclipse Persistence Tools
+ */
+//@JsonIdentityInfo(
+//		scope= UserSessionPOJO.class,
+//		generator=ObjectIdGenerators.PropertyGenerator.class,
+//		property = "id")
+//@Entity
+//@Table(name = "USER_SESSION", catalog = "C3")
+public class UserSessionDTO extends Dto {
+
+	// Fields
+
+//	@Id
+//	@GeneratedValue(strategy = IDENTITY)
+//	@Column(name = "ID", unique = true, nullable = false)
+	public Long id;
+
+//	@Column(name = "UserId", nullable = false)
+	private Long userId;
+
+//	@Column(name = "LoginTime", nullable = true)
+	private Timestamp loginTime;
+
+	// Constructors
+
+	/** default constructor */
+	public UserSessionDTO() {
+	}
+
+	// Property accessors
+
+	@SuppressWarnings("unused")
+	public Long getUserId() {
+		return this.id;
+	}
+
+	@SuppressWarnings("unused")
+	public void setUserId(Long id) {
+		this.id = id;
+	}
+
+	@SuppressWarnings("unused")
+	public Long getId() {
+		return id;
+	}
+
+	@SuppressWarnings("unused")
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@SuppressWarnings("unused")
+	public Timestamp getLoginTime() {
+		return loginTime;
+	}
+
+	@SuppressWarnings("unused")
+	public void setLoginTime(Timestamp loginTime) {
+		this.loginTime = loginTime;
+	}
+}
