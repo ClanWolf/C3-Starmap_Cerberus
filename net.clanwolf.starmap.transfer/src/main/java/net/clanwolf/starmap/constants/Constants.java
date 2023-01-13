6430838c5019304c406c10e922fc2f008de5a4a1
WarWolfen <warwolfen@gmail.com>
2023-01-13 22:19:29 +0100

    Version 7.2.19 (13.01.2023) - FIXED: Motivational voice samples should now match the drop results in invasion drops

--- net.clanwolf.starmap.transfer/src/main/java/net/clanwolf/starmap/constants/Constants.java
+++ net.clanwolf.starmap.transfer/src/main/java/net/clanwolf/starmap/constants/Constants.java
@@ -91,7 +91,7 @@ public class Constants implements java.io.Serializable {
     public static final long ROLE_ATTACKER_COMMANDER = 1L; // 1L Attacker Commander
     public static final long ROLE_DEFENDER_WARRIOR = 2L; // 2L Defender
     public static final long ROLE_DEFENDER_COMMANDER = 3L; // 3L Defender Commander
-    public static final long ROLE_SUPPORTER = 4L; // 4L Attacker Supporter
+    public static final long ROLE_SUPPORTER = 4L; // 4L Supporter
     public static final long ROLE_ATTACKER_SUPPORTER = 5L; // 4L Attacker Supporter
     public static final long ROLE_DEFENDER_SUPPORTER = 6L; // 5L Defender Supporter
 
