cba82d904e7864b0e47f0646b41657d6dc48269c
WarWolfen <warwolfen@gmail.com>
2023-01-16 20:13:14 +0100

    Version 7.2.21 (15.01.2023) - FIXED: Error in db selection

--- net.clanwolf.starmap.server/src/main/php/insert_new_version.php
+++ net.clanwolf.starmap.server/src/main/php/insert_new_version.php
@@ -23,10 +23,10 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "UPDATE C3.SYSCONFIG SET VALUE='7.2.21' WHERE SYSCONFIG.KEY='VERSION CLIENT'";
+$sql = "UPDATE C3.SYSCONFIG SET VALUE='7.2.22' WHERE SYSCONFIG.KEY='VERSION CLIENT'";
 
 if ($conn->query($sql) === TRUE) {
-  echo "Record updated successfully to 7.2.21.";
+  echo "Record updated successfully to 7.2.22.";
 } else {
   echo "Error updating record: " . $conn->error;
 }
