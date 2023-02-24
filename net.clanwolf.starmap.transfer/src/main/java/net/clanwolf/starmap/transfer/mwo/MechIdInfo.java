85094d7d6ec72bb455b9a3d0134830d45c02fef0
Kernreaktor <mail-an-kernreaktor@web.de>
2023-02-24 22:17:25 +0100

    ADDED: 4 IS Mechs (TBT-7K(P), ASN-27(P), HSN-8P(P), STK-5M(P)) to MechIdInfo Class

--- net.clanwolf.starmap.transfer/src/main/java/net/clanwolf/starmap/transfer/mwo/MechIdInfo.java
+++ net.clanwolf.starmap.transfer/src/main/java/net/clanwolf/starmap/transfer/mwo/MechIdInfo.java
@@ -1022,6 +1022,10 @@ public class MechIdInfo {
         this.mechID.put(937, new MechIdInfo(EFaction.CLAN, EChassie.ICEFERRET, EVariantType.SPECIAL, "ICEFERRET IFR-P(P)", "IFR-P(P)"));
         this.mechID.put(938, new MechIdInfo(EFaction.CLAN, EChassie.EBONJAGUAR, EVariantType.SPECIAL, "EBONJAGUAR EBJ-A(P)", "EBJ-A(P)"));
         this.mechID.put(939, new MechIdInfo(EFaction.CLAN, EChassie.BLOODASP, EVariantType.SPECIAL, "BLOODASP BAS-B(P)", "BAS-B(P)"));
+        this.mechID.put(940, new MechIdInfo(EFaction.INNERSPHERE, EChassie.TREBUCHET, EVariantType.SPECIAL, "TREBUCHET TBT-7K(P)", "TBT-7K(P)"));
+        this.mechID.put(941, new MechIdInfo(EFaction.INNERSPHERE, EChassie.ASSASSIN, EVariantType.SPECIAL, "ASSASSIN ASN-27(P)", "ASN-27(P)"));
+        this.mechID.put(942, new MechIdInfo(EFaction.INNERSPHERE, EChassie.HELLSPAWN, EVariantType.SPECIAL, "HELLSPAWN HSN-8P(P)", "HSN-8P(P)"));
+        this.mechID.put(943, new MechIdInfo(EFaction.INNERSPHERE, EChassie.STALKER, EVariantType.SPECIAL, "STALKER STK-5M(P)", "STK-5M(P)"));
         this.mechID.put(990, new MechIdInfo(EFaction.INNERSPHERE, EChassie.ATLAS, EVariantType.SPECIAL, "ATLAS AS7-D-DC", "AS7-D-DC"));
         this.mechID.put(996, new MechIdInfo(EFaction.INNERSPHERE, EChassie.JENNER, EVariantType.FOUNDER, "JENNER JR7-D(F)", "JR7-D(F)"));
         this.mechID.put(997, new MechIdInfo(EFaction.INNERSPHERE, EChassie.CATAPULT, EVariantType.FOUNDER, "CATAPULT CPLT-C1(F)", "CPLT-C1(F)"));
