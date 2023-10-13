db9159ee8ff1c81cafc5866648a9a4f7f717e49a
WarWolfen <warwolfen@gmail.com>
2023-10-14 01:57:32 +0200

    Version 7.4.3 (14.10.2023) - CHANGED: Only one alliance is allowed for each faction at a time - CHANGED: Mouse cursor general - FIXED: Crash on start if a new version is online

--- net.clanwolf.starmap.server/src/main/php/insert_new_version.php
+++ net.clanwolf.starmap.server/src/main/php/insert_new_version.php
@@ -23,10 +23,10 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "UPDATE C3.SYSCONFIG SET VALUE='7.4.2' WHERE SYSCONFIG.KEY='VERSION CLIENT'";
+$sql = "UPDATE C3.SYSCONFIG SET VALUE='7.4.3' WHERE SYSCONFIG.KEY='VERSION CLIENT'";
 
 if ($conn->query($sql) === TRUE) {
-  echo "Record updated successfully to 7.4.2.";
+  echo "Record updated successfully to 7.4.3.";
 } else {
   echo "Error updating record: " . $conn->error;
 }
